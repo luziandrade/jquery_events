963e400c10d662c452d194f12de63886ffc22d75
Luana Andrade <luanajomi@gmail.com>
2019-06-02 09:23:31 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+ 
+$("button").mouseenter(function() {
+		$(this).removeClass("makeRed").addClass("makeBorder");
+	});
+
+	$("button").mouseleave(function() {
+		$("button").removeClass("makeBorder").addClass("makeRed");
+	});
+
+}); 
